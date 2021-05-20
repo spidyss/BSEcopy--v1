e2c1bab00848e6f62a6abe77c4c21ab5fbe1f640
spidyss <61074365+spidyss@users.noreply.github.com>
2021-05-20 23:46:31 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,16 +5,10 @@
 const path = require('path')
 
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000  # Fall back to port 5000 if process.env.PORT is not set
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
 
 module.exports = {
