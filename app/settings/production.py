17ca85230f69cfd689fec2cc9e30ecbb4f43b7ef
spidyss <61074365+spidyss@users.noreply.github.com>
2021-05-20 20:36:20 +0530

    Update production.py

--- app/settings/production.py
+++ app/settings/production.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR, 'dist/static']
