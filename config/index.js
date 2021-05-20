f084d46262fe297ef63fb77ebf1510b697cc4890
spidyss <61074365+spidyss@users.noreply.github.com>
2021-05-20 23:50:18 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,11 +4,6 @@
 
 const path = require('path')
 
-const express = require('express')
-const PORT = process.env.PORT || 5000  # Fall back to port 5000 if process.env.PORT is not set
-
-express()
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 module.exports = {
